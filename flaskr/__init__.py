36f9b668ed8cb1dbc79b178f6b7d0151323fbd42
LuanaLopez20 <luanaslopez@outlook.com>
2025-05-12 10:51:28 -0300

    Definir y acceder a la base de datos

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,9 +2,8 @@ import os
 
 from flask import Flask
 
-
 def create_app(test_config=None):
-    # create and configure the app
+    #Crear y configurar la aplicación
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -12,21 +11,24 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
+        #Cargar la configuración de la instancia, si existe, cuando no se está probando
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
+        #Cargar la configuración de prueba si se pasa en
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
+    #Asegúrese de que la carpeta de la instancia exista
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # a simple page that says hello
+    #Una página simple que dice hola
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
 
     return app
